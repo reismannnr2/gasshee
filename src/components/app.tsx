442ff3bf48cba1511aa72e2b38207c1134c76fe3
reismannnr2 <reismannnr2@gmail.com>
2022-02-04 15:48:05 +0900

    current source

---
+++ src/components/app.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+
+const App = () => {
+  return <h1 css={css({})}>Hello, Gasshee!</h1>;
+};
+
+export default App;
